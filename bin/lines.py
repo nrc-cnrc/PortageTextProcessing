c4f47b53e24ab63ebb94d1142a12b5e6bf645ae5
Nicola Ueffing <>
2008-01-21 20:21:55 +0000

    fixed some errors I introduced before

--- bin/lines.py
+++ bin/lines.py
@@ -21,7 +21,7 @@ if len(sys.argv)!=3:
     <file containing line numbers>  <file containing text (can be gzipped)>\n\n\
     Extracts lines specified in first file from second file.\n\
     Line numbers have to start with 1 (not 0) and may contain repititions.\n\
-    " % (sys.argv[0])))
+    " % sys.argv[0])
     sys.exit(1)
 
 ### Read arguments
@@ -40,21 +40,21 @@ while line!="":
     line = numFile.readline()
 nums.sort()
 
-    ### read text and extract lines
-    n1 = 1
-    n2 = nums.pop(0)
+### read text and extract lines
+n1 = 1
+n2 = nums.pop(0)
+line = txtFile.readline()
+while (line!="") & (len(nums)>0):
+    #print "%",n1,n2
+    while (n1 == n2) & (len(nums)>0):
+        #sys.stderr.write("Line %i: %s\n" % (n2,line))
+        print line,
+        n2 = nums.pop(0)
+    #print "#",n1,line,
+    n1 = n1+1
     line = txtFile.readline()
-    while (line!="") & (len(nums)>0):
-        #print "%",n1,n2
-        while (n1 == n2) & (len(nums)>0):
-            sys.stderr.write("Line %i: %s\n" % (n2,line))
-            print line,
-            n2 = nums.pop(0)
-        #print "#",n1,line,
-        n1 = n1+1
-        line = txtFile.readline()
 
-    while (line!="") & (n1<n2):
-        line = txtFile.readline()
-        n1 = n1+1
-    print line,
+while (line!="") & (n1<n2):
+    line = txtFile.readline()
+    n1 = n1+1
+print line,
