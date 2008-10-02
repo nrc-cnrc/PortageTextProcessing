0062ce0679c685003f7ee00d9fe94fc5ba7db1f8
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2008-10-02 16:42:12 +0000

    New method for running all test suites in a single command.

---
+++ tests/run-all-tests.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# run-all-tests.sh - Run all unit testing suites, reporting which ones failed
+#
+# PROGRAMMER: Eric Joanis
+#
+# Technologies langagieres interactives / Interactive Language Technologies
+# Inst. de technologie de l'information / Institute for Information Technology
+# Conseil national de recherches Canada / National Research Council Canada
+# Copyright 2008, Sa Majeste la Reine du Chef du Canada /
+# Copyright 2008, Her Majesty in Right of Canada
+
+if [[ "$1" =~ "^-" ]]; then
+   echo "Usage: $0 [test-suite [test-suite2 [...]]]
+       Run specified test suites, or all test suites if none are specified"
+   exit
+fi
+
+TEST_SUITES=$*
+if [[ ! $TEST_SUITES ]]; then
+   TEST_SUITES=`echo */run-test.sh | sed 's/\/run-test.sh//g'`
+fi
+
+echo ""
+echo Test suites to run: $TEST_SUITES
+
+for TEST_SUITE in $TEST_SUITES; do
+   echo ""
+   echo =======================================
+   echo Running $TEST_SUITE
+   if cd -- $TEST_SUITE; then
+      if [[ ! -x ./run-test.sh ]]; then
+         echo FAILED $TEST_SUITE: can\'t find or execute ./run-test.sh
+         FAIL="$FAIL $TEST_SUITE"
+      elif ./run-test.sh >& log.run-test; then
+         echo PASSED $TEST_SUITE
+      else
+         echo FAILED $TEST_SUITE: ./run-test.sh returned $?
+         FAIL="$FAIL $TEST_SUITE"
+      fi
+
+      cd ..
+   else
+      echo FAILED $TEST_SUITE: could not cd into $TEST_SUITE
+      FAIL="$FAIL $TEST_SUITE"
+   fi
+done
+
+echo ""
+echo =======================================
+if [[ $FAIL ]]; then
+   echo FAILED these test suites:$FAIL
+   exit 1
+else
+   echo PASSED all test suites
+fi
+
+
+
