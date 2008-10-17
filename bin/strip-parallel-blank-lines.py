10192202c4da83543bc55b1fd26e9b381fece18a
George Foster <>
2008-10-17 20:33:02 +0000

    Added dopy option for GALE preproc

--- bin/strip-parallel-blank-lines.py
+++ bin/strip-parallel-blank-lines.py
@@ -17,16 +17,28 @@ import sys
 import re
 
 help = """
-strip-parallel-blank-lines.py file1 file2 [file3...]
+strip-parallel-blank-lines.py [-r] file1 file2 [file3...]
 
 Strip blank lines in parallel from two line-aligned files: strip if EITHER file
 contains a blank line.  Write output to <file1>.no-blanks. If <file3> and subsequent
 files are provided, they are stripped in parallel, but are not themselves checked
 for blank lines.
 
+Options:
+-r   For any set of parallel lines in which the first (ONLY!) is blank, replace
+     any blank lines with a '.'. NOTE the change of matching criterion with
+     this option! 
+
 """
 
-if len(sys.argv) < 1+2:
+args = sys.argv[1:]
+
+replace = False
+if len(args) > 1 and args[0] == "-r":
+    replace = True
+    args = args[1:]
+
+if len(args) < 2:
     sys.stderr.write(help);
     sys.exit(1)
     
@@ -34,7 +46,7 @@ blankline = re.compile("^\s*$")
 
 ifiles = []
 ofiles = []
-for file in sys.argv[1:]:
+for file in args:
     ifiles.append(open(file, "r"))
     ofiles.append(open(file+".no-blanks", "w"))
 
@@ -47,9 +59,15 @@ for lines[0] in ifiles[0]:
             sys.stderr.write("file " + ifiles[i].name + " too short!\n")
             sys.exit(1)
 
-    if (not blankline.match(lines[0]) and not blankline.match(lines[1])):
+    if replace:
+        rep = blankline.match(lines[0])
         for i in range(0,len(ifiles)):
+            if rep and blankline.match(lines[i]): lines[i] = ".\n"
             ofiles[i].write(lines[i])
+    else:
+        if not blankline.match(lines[0]) and not blankline.match(lines[1]):
+            for i in range(0,len(ifiles)):
+                ofiles[i].write(lines[i])
 
 for file in ifiles:
     if (file.readline() != ""):
