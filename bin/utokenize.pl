2318aa6ecac7c7db1564ec5e8485d7c024ffd734
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2008-11-07 15:08:19 +0000

    Replace remove-standard-escapes.pl by canoe-escapes.pl, which supports both the -add and the -remove operations. Plus minor documentation patches while migrating stuff to PORTAGEshared.

--- bin/utokenize.pl
+++ bin/utokenize.pl
@@ -7,8 +7,8 @@
 # PROGRAMMER: George Foster, with minor modifications by Aaron Tikuisis
 #             UTF-8 adaptation by Michel Simard.
 #
-# Copyright (c) 2004 - 2007, Sa Majeste la Reine du Chef du Canada /
-# Copyright (c) 2004 - 2007, Her Majesty in Right of Canada
+# Copyright (c) 2004 - 2008, Sa Majeste la Reine du Chef du Canada /
+# Copyright (c) 2004 - 2008, Her Majesty in Right of Canada
 #
 # This software is distributed to the GALE project participants under the terms
 # and conditions specified in GALE project agreements, and remains the sole
