c7dd6279e35a2376a672c19e412fbb07a2e4fe80
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2009-12-14 21:29:20 +0000

    Adding a unit-test for second-to-hms.pl. (Initial version)

---
+++ tests/second-to-hms.pl/run-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# $Id$
+
+# @file run-test.sh
+# @brief Run this test suite, with a non-zero exit status if it fails
+#
+# @author Samuel Larkin
+#
+# Technologies langagieres interactives / Interactive Language Technologies
+# Inst. de technologie de l'information / Institute for Information Technology
+# Conseil national de recherches Canada / National Research Council Canada
+# Copyright 2009, Sa Majeste la Reine du Chef du Canada /
+# Copyright 2009, Her Majesty in Right of Canada
+
+make -j testsuite
+
+exit
