1660f8a39a08b949ebf1d229daa0341a00f37e31
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2011-12-14 16:36:16 +0000

    Initial version.

---
+++ tests/portage_utils.py/run-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# run-test.sh - Unit-test for portage_utils.pyc
+# Tests
+#
+# PROGRAMMER: Samuel Larkin
+#
+# Technologies langagieres interactives / Interactive Language Technologies
+# Inst. de technologie de l'information / Institute for Information Technology
+# Conseil national de recherches Canada / National Research Council Canada
+# Copyright 2011, Sa Majeste la Reine du Chef du Canada /
+# Copyright 2011, Her Majesty in Right of Canada
+
+\make clean
+\make all -B
+
+exit
