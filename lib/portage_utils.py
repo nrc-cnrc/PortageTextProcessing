173947f3e48cf245f9d59293f978a52468e50a9e
Darlene Stewart <Darlene.Stewart@nrc-cnrc.gc.ca>
2012-03-13 13:39:48 +0000

    Changed warning prefix from "Warn:" to "Warning:".

--- lib/portage_utils.py
+++ lib/portage_utils.py
@@ -105,7 +105,7 @@ def fatal_error(*args):
 
 def warn(*args):
     """Print an warning message to stderr."""
-    print("Warn:", *args, file=sys.stderr)
+    print("Warning:", *args, file=sys.stderr)
     return
 
 def info(*args, **kwargs):
