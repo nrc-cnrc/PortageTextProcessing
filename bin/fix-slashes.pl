19982855ae743d963a95a42735e2c4dfa671b61b
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2014-06-25 14:50:29 -0400

    Add fix-slashes.pl and an example how to call it.
    (cherry picked from commit PORTAGEshared/546d3773f25c87ece9f3ce2ce29cbf7adc47a270)

---
+++ bin/fix-slashes.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+#
+# @file fix-slashes.pl
+# @brief Heuristically add whitespace around slashes ('/') seperating words.
+#
+# @author Darlene Stewart
+#
+# COMMENTS:
+#
+# Traitement multilingue de textes / Multilingual Text Processing
+# Tech. de l'information et des communications / Information and Communications Tech.
+# Conseil national de recherches Canada / National Research Council Canada
+# Copyright 2010-2014, Conseil national de recherches du Canada /
+# Copyright 2010-2014, National Research Council of Canada
+
+
+use warnings;
+use encoding "UTF-8";
+
+# Add whitespace around slashes ('/') separating words of at least three characters.
+# Skip anything that looks like a url.
+# We make it at least three characters in an attempt to avoid breaking up
+# specifications of units (e.g. km/hr).
+
+my $word = "(?:(?:[[:alpha:]][[:alpha:]\-]{2,})|(?:[[:alpha:]][[:alpha:]\-]*'[[:alpha:]]+))";
+
+while (my $line = <>) {
+    chop $line;
+    while ($line =~ m/(?:^|[ ("'])(?!www\.)(${word}\)?(((\/ *)|( \/))\(?${word})+)(?:[ ).?;,:"']|$)/g) {
+       my ($pre, $match, $post) = ($`, $&, $');
+       $match =~ s/ *\/ */ \/ /g;
+       $line = $pre . $match . $post;
+    }
+    $line =~ s/CAN \/ /CAN\//g;  # Patch CAN document #s, e.g. CAN/CSA B44-2000
+    print $line, "\n";
+}
+
