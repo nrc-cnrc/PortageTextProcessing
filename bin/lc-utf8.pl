a621e1a46d02bc8b5c3f863298ce3f23e853704f
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2016-07-11 13:09:14 -0400

    Using user's perl instead of system's perl.
    (cherry picked from PORTAGEshared commit a4cf7dc2992e4ba5647a0e5aa3a3b47d71aab5da)

--- bin/lc-utf8.pl
+++ bin/lc-utf8.pl
@@ -1,4 +1,8 @@
-#!/usr/bin/perl -s
+#!/bin/sh
+#! -*-perl-*-
+eval 'exec perl -x -s -wS $0 ${1+"$@"}'
+   if 0;
+
 
 # @file lc-utf8.pl 
 # @brief Lowercase mapping for utf-8
--- bin/stableuniq.pl
+++ bin/stableuniq.pl
@@ -1,4 +1,9 @@
-#!/usr/bin/perl -sw
+#!/bin/sh
+#! -*-perl-*-
+eval 'exec perl -x -s -wS $0 ${1+"$@"}'
+   if 0;
+
+use warnings;
 
 # @file stableuniq.pl 
 # @brief Output unique lines from INPUT (or standard input) to OUTPUT (or
