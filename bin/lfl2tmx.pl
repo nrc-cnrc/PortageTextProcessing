6a91463ce206e9fa170b6080b328c240d4d1b2b9
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2021-01-21 11:56:23 -0500

    Bug fix: lfl2tmx.pl exit 0 on -h, 1 on error

--- bin/lfl2tmx.pl
+++ bin/lfl2tmx.pl
@@ -56,7 +56,7 @@ Options:
   -v(erbose)    increment the verbosity level by 1 (may be repeated)
   -d(ebug)      print debugging information
 ";
-   exit $_ ? 1 : 0;
+   exit @_ ? 1 : 0;
 }
 
 use Getopt::Long;
