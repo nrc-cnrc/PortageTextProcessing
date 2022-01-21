8081a141d8c90c5b153c052271d66e29c80cfa2b
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2022-01-21 12:11:40 -0500

    make open_newlines_encodings.py more robust on GPSC
     - put the output files on /tmp, so it's a local filesystem, and thus faster
     - sleep 0.2s instead of 0.1, because otherwise the test suite sometimes fails on GPSC.

--- tests/portage_utils.py/open_newlines_encodings.py
+++ tests/portage_utils.py/open_newlines_encodings.py
@@ -3,6 +3,7 @@
 import builtins
 import os
 import portage_utils
+import tempfile
 import time
 from portage_utils import open
 
@@ -27,32 +28,39 @@ for infile in "newlines.txt", "newlines.gz", "cat newlines.txt |":
     )
 
 
-for outfile, result in (
-    ("outlines.txt", "outlines.txt"),
-    ("outlines.gz", "outlines.gz"),
-    ("| cat > outlines.txt", "outlines.txt")
-):
-    for encoding in "latin1", "utf8", portage_utils.DEFAULT_ENCODING_VALUE:
-        for newline in None, "", "\n", "\r", "\r\n":
-            with open(outfile, "w", encoding=encoding, newline=newline) as f:
-                print("àéCR\rCRLF\r", file=f)
-                print("LF", file=f)
-            time.sleep(0.1)
-            with open(result, "rb") as res:
-                print(
-                    outfile,
-                    "default" if encoding is portage_utils.DEFAULT_ENCODING_VALUE else encoding,
-                    repr(newline),
-                    repr(res.read()),
-                )
-    with open(outfile, "wb") as f:
-        f.write(b"CR\rCRLF\r\nLF\n")
-    time.sleep(0.1)
-    with open(result, "rb") as res:
-        print(
-            outfile,
-            "wb",
-            repr(res.read()),
-        )
+tmp_dir_root = "/tmp" if os.path.exists("/tmp") else None  # For portability
+with tempfile.TemporaryDirectory(dir=tmp_dir_root) as tmpdirname:
+    for outfile, result in (
+        ("outlines.txt", "outlines.txt"),
+        ("outlines.gz", "outlines.gz"),
+        ("| cat > outlines.txt", "outlines.txt")
+    ):
+        if outfile == "| cat > outlines.txt":
+            outfile_path = "| cat > " + os.path.join(tmpdirname, "outlines.txt")
+        else:
+            outfile_path = os.path.join(tmpdirname, outfile)
+        result_path = os.path.join(tmpdirname, result)
+        for encoding in "latin1", "utf8", portage_utils.DEFAULT_ENCODING_VALUE:
+            for newline in None, "", "\n", "\r", "\r\n":
+                with open(outfile_path, "w", encoding=encoding, newline=newline) as f:
+                    print("àéCR\rCRLF\r", file=f)
+                    print("LF", file=f)
+                time.sleep(0.2)
+                with open(result_path, "rb") as res:
+                    print(
+                        outfile,
+                        "default" if encoding is portage_utils.DEFAULT_ENCODING_VALUE else encoding,
+                        repr(newline),
+                        repr(res.read()),
+                    )
+        with open(outfile_path, "wb") as f:
+            f.write(b"CR\rCRLF\r\nLF\n")
+        time.sleep(0.2)
+        with open(result_path, "rb") as res:
+            print(
+                outfile,
+                "wb",
+                repr(res.read()),
+            )
 
 
